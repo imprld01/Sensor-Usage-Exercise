3073d4c56472d8192ccd1ec2836cd08a6e30c3fa
Bo-Wun Shih <bowns1688@hotmail.com>
2017-05-22 00:08:11 +0800

    [change] update value retrieval for x, y, z

--- app/src/main/java/bowns/listener/AccelerometerListener.java
+++ app/src/main/java/bowns/listener/AccelerometerListener.java
@@ -20,9 +20,21 @@ public class AccelerometerListener implements SensorEventListener {
         acc.getVendor();
 
         float [] gValues = event.values;
+        float [] gravity = new float[3];
+        float [] linear_acceleration = new float[3];
 
         float x = gValues[0];
         float y = gValues[1];
         float z = gValues[2];
+
+        final float alpha = (float)0.8;
+
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        linear_acceleration[0] = event.values[0] - gravity[0];
+        linear_acceleration[1] = event.values[1] - gravity[1];
+        linear_acceleration[2] = event.values[2] - gravity[2];
     }
 }
